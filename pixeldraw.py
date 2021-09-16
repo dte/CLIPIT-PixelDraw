ccb9d798dc9acaf5786e72b71101166ebfc357c2
Dillon Erb <585865+dte@users.noreply.github.com>
2021-09-16 18:25:59 -0400

    Update pixeldraw.py

--- pixeldraw.py
+++ pixeldraw.py
@@ -13,7 +13,7 @@ sys.path.append("/taming-transformers")
 #Run the script
 
 # prompts = "Darkness cannot drive out darkness. Only light can do that. #pixelart" #@param {type:"string"}
-prompts = "Party Parrot Party #pixelart" #@param {type:"string"}
+prompts = "Party parrot DJs a nightclub graffiti warehouse party in Brooklyn #pixelart" #@param {type:"string"}
 
 aspect = "widescreen" #@param ["widescreen", "square"]
 use_pixeldraw = True #@param {type:"boolean"}
